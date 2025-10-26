0c9e8c98fdff79ce1bb3621628740a4eedd43e47
fearyfiremast <fearyfiremast@gmail.com>
2025-10-25 17:49:02 -0700

    successfully getting resource from proxy server using curl

--- http_server_test.py
+++ http_server_test.py
@@ -1,20 +1,34 @@
 import unittest
+import subprocess
 import sys
 
 # Declarations
-SOCKET = None
 REPORT_STATUS = False # if value is true write report
 PORT = 8080
+HOST = "127.0.0.1"
+DESTINATION = None
+RESOURCE = "/test.html"
 
 class TestPart1(unittest.TestCase):
+    request = None
+
     def setUp(self):
-        return super().setUp()
+       global DESTINATION
+       DESTINATION = f"http://{HOST}:{PORT}{RESOURCE}"
     
     def tearDown(self):
         return super().tearDown()
     
     def test_GET_method(self):
-        print("test run")
+        # test does not need bespoke 
+        cmd = [
+            "curl",
+            "-i",
+            f"{DESTINATION}"
+        ]
+        
+        result = subprocess.run(cmd, capture_output=True, text=True)
+        print(result)
         return
 
 def refreshReport():
@@ -31,9 +45,12 @@ def appendReport():
 # entry point
 # specific tests can be ran from the command line: https://docs.python.org/3/library/unittest.html
 # If report or port number is wanted it is not recommended to use the interface from above link
+
 if __name__ == "__main__":
     if len(sys.argv) > 1:
+        # may cause errors with unittest sys.argv implementation
         if sys.argv[1].isdigit() and 0 < int(sys.argv[1]) < 65536:
             PORT = int(sys.argv[1])
-    
+            del sys.argv[1]
+
     unittest.main()
