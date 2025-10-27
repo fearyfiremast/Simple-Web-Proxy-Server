c9678ebd2885a6f13923e5d1cdbd3c9110a7fb65
fearyfiremast <fearyfiremast@gmail.com>
2025-10-27 13:37:34 -0700

    filled out logic of find record function. Added helper functions that need to be filled out

--- cache_utils.py
+++ cache_utils.py
@@ -1,6 +1,6 @@
 """Module that handles server cache behaviour"""
 import threading
-from message_utils import get_date_header, convert_datetime_to_posix
+from message_utils import is_not_modified_since
 
 class Cache:
     '''
@@ -14,22 +14,64 @@ class Cache:
     def __init__(self):
         return
     
-    def _validate_expiry(self, ):
-        #  may cause de
-        with self._lock:
-            if len(self._records) == 0:
-                return
+    def _is_expired(self, record):
+        """
+        TODO
+        method that takes as input a record and checks to see if it has expired.
+        
+        Args:
+            record (Record): the record that may or may not have expired
+
+        Returns:
+            (bool): True if it has expired, false otherwise
+        """
             
         return
     
-    def is_cached(self):
+    def _remove_records(self, array):
+        """
+        TODO: Removes records from _records list
+        """
         return
     
-    def find_record(self):
-        with self._lock:
-            return
-        
+    def _record_to_response(self, record):
+        """
+        TODO: Converts the cache representation of data to socket representation
+        """
         return
+    
+    def _response_to_record(self):
+        """
+        TODO: Converts the socket representation of data to cache representation
+        """
+        return
+
+    def find_record(self, key):
+        to_return = None
+        expired_records = []
+
+        with self._lock:
+            if len(self._records) == 0:
+                return None
+            
+            for record in self._records:
+
+                # If the record in cache is expired, it will be marked for removal somehow
+                if self._is_expired(record):
+                    expired_records.append(record)
+                    continue # prevents the case of a record being expired and matching the key
+                
+                if record.is_match(key):
+                    to_return = record
+                    to_return.update_expiry_date()
+                    self._records.remove(record)
+                    # Propends record to front to emulate temporal locality
+                    self._records = [to_return, self._records] 
+                    break # We found the record that we wanted so we leave early. 
+                    
+            self._remove_records(expired_records)
+ 
+        return to_return
     
     def insert_record(self):
         with self._lock:
@@ -49,7 +91,10 @@ class Record:
         self._modification_date = modifaction_date
         self._data = data
 
+    def update_expiry_date(self):
+        return
 
-    def is_resource(self, request) -> bool:
+
+    def is_match(self, key) -> bool:
         """Checks if the record has the resource by URL"""
-        return self._url == request
+        return self._url == key
